0614c7f9416a74d4d57d51bf791c663e49d52d6f
sxh705 <2362989228@qq.com>
2024-01-05 19:58:22 +0800

    Initial commit

---
+++ day1走进golang.go
@@ -0,0 +1,10 @@
+package main // 每个go文件都必须有归属包,主函数必须属于main包
+import "fmt" // 为了使用包下的函数, 引入程序所需的包
+
+func main() { // main 主函数 入口
+	fmt.Println("hello" + "world") // 在控制台打印一句话
+}
+
+// 编译使用 go build day1 xxx.go
+
+// 运行使用 go run day1 xxx.go
